ba528630a2f1a48948c9a1a09f3ad43340a8ab87
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-01-13 14:10:36 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dragonboard',
+    version='0.0.1',
+    description='A reader library for the cta dragonboard data',
+    url='http://github.com/mackaiver/lst_calibration',
+    author='Kai Brügge, Dominik Neise, Maximilian Nöthe',
+    author_email='kai.bruegge@tu-dortmund.de',
+    license='MIT',
+    packages=['dragonboard'],
+)
