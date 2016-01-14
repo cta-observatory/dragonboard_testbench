48c1d501c134ace157063f3d5ed9a16592fa009c
Mario <mario.hoerbe@rub.de>
2016-01-14 17:13:43 +0100

    now able to calculate mean offset

--- offset_calculation.py
+++ offset_calculation.py
@@ -6,33 +6,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from dragonboard import EventGenerator
 
+
 def offset_calc(filename):
-	with open(filename, "rb") as f:
-		generator = EventGenerator(f, max_events=20)
-		#event = next(generator)
-		#plt.plot(event.data["low"][0])
-		#plt.show()
-		
-		# declare events which is a list of all generator events
-		events=list(generator)
-		
-		# read out list length
-		numevents=len(events)
-	
-		# initialize an array full of nans. length = number of caps, width = number of events
-		pedestalvalues = np.full((numevents, 4096),np.nan)
-		
-		#
-		pixelindex = 0
-		
-		for i in range(numevents):
-			stopcell = events[i].header.stop_cells[pixelindex]
-			roi = events[i].roi
-			pedestalvalues[i][stopcell:stopcell+roi] = events[i].data["low"][pixelindex]
-			
-			plt.plot(pedestalvalues[i],".:")
-			
-		plt.show()
-		
+    with open(filename, "rb") as f:
+        generator = EventGenerator(f, max_events=20)
+        #event = next(generator)
+        # plt.plot(event.data["low"][0])
+        # plt.show()
+
+        # declare events which is a list of all generator events
+        events = list(generator)
+
+        # read out list length
+        numberofevents = len(events)
+
+        # initialize an array full of nans. length = number of caps, width = number of events
+        pedestalvalues = np.full((numberofevents, 4096), np.nan)
+
+        #
+        pixelindex = 0
+
+        # build an array with numberofevents rows and 4096 columns. each event is then implemented into a big matrix
+        for i in range(numberofevents):
+            stopcell = events[i].header.stop_cells[pixelindex]
+            roi = events[i].roi
+            pedestalvalues[i][stopcell:stopcell + roi] = events[i].data["low"][pixelindex]
+
+        # calculate and plot means
+        plt.figure()
+        plt.errorbar(
+            np.arange(4096), 
+            np.nanmean(pedestalvalues, axis=0),
+            yerr=np.nanstd(pedestalvalues, axis=0),
+            fmt="+",
+            markersize=3,
+            capsize=0,
+        )
+        plt.xlim(0,4096)      
+        plt.show()
+
 if __name__ == '__main__':
-	offset_calc('Ped444706_1.dat')
+    offset_calc('Ped444706_1.dat')
