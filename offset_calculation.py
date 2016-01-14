c685c48e6701f4780c285eed77d9a521cb1f7139
Mario <mario.hoerbe@rub.de>
2016-01-14 18:20:34 +0100

    more comments added, file gives table with mean and RMS out

--- offset_calculation.py
+++ offset_calculation.py
@@ -1,48 +1,44 @@
-# 1 pedestal_file öffnen
-# plotten?
-# mittelwerte berechnen
+# programm calculates statistics (mean offsets) on the fly and creates a list with all data.
+# all channes and low/gain has to be implemented!
 
 import numpy as np
 import matplotlib.pyplot as plt
-from dragonboard import EventGenerator
-
+from dragonboard import EventGenerator # custom event generator
+from dragonboard.runningstats import RunningStats # Max's method to do statistics on the fly 
+from tqdm import tqdm # enable to show progress
 
 def offset_calc(filename):
-    with open(filename, "rb") as f:
-        generator = EventGenerator(f, max_events=20)
-        #event = next(generator)
-        # plt.plot(event.data["low"][0])
-        # plt.show()
+    with open(filename, "rb") as f: # open a file and call it f. "rb": read, file is binary
+        generator = EventGenerator(f) #, max_events=50) #  
+        next(generator) # leave the first event as it is reasonably shifted to low caps
 
-        # declare events which is a list of all generator events
-        events = list(generator)
+        # 
+        stats = RunningStats(shape=4096)
 
-        # read out list length
-        numberofevents = len(events)
-
-        # initialize an array full of nans. length = number of caps, width = number of events
-        pedestalvalues = np.full((numberofevents, 4096), np.nan)
-
-        #
+        # define a pixel index.
         pixelindex = 0
 
-        # build an array with numberofevents rows and 4096 columns. each event is then implemented into a big matrix
-        for i in range(numberofevents):
-            stopcell = events[i].header.stop_cells[pixelindex]
-            roi = events[i].roi
-            pedestalvalues[i][stopcell:stopcell + roi] = events[i].data["low"][pixelindex]
+        # build an array with 4096 columns. calculate mean using Max's method
+        for event in tqdm(generator):
+            data = np.full(4096, np.nan)
+            stop_cell = event.header.stop_cells[pixelindex]
+            roi = event.roi
+            data[:roi] = event.data["low"][pixelindex]
+            stats.add(np.roll(data, stop_cell))
 
-        # calculate and plot means
+        np.savetxt('offsets.csv', np.column_stack([stats.mean, stats.std]), delimiter=',')
+
+        # plot means
         plt.figure()
         plt.errorbar(
             np.arange(4096), 
-            np.nanmean(pedestalvalues, axis=0),
-            yerr=np.nanstd(pedestalvalues, axis=0),
-            fmt="+",
+            stats.mean,
+            yerr=stats.std, # yerr = y-error bars
+            fmt="+", # fmt means format
             markersize=3,
-            capsize=0,
+            capsize=0, # frame bars of error bars
         )
-        plt.xlim(0,4096)      
+        # plt.xlim(0,4096)      
         plt.show()
 
 if __name__ == '__main__':
