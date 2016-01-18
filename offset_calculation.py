94d23fa4d62416420a990293f8540faee2f6cf69
Mario <mario.hoerbe@rub.de>
2016-01-18 15:11:48 +0100

    added ability to calculate and plot mean offsets and RMS of all channels both at low and high gain

--- offset_calculation.py
+++ offset_calculation.py
@@ -7,39 +7,45 @@ from dragonboard import EventGenerator # custom event generator
 from dragonboard.runningstats import RunningStats # Max's method to do statistics on the fly 
 from tqdm import tqdm # enable to show progress
 
-def offset_calc(filename):
+# calculate mean offset & RMS for every capacitor and plot the data.
+def offset_calc(filename, pixelindex, gaintype):
     with open(filename, "rb") as f: # open a file and call it f. "rb": read, file is binary
-        generator = EventGenerator(f) #, max_events=50) #  
+        generator = EventGenerator(f, max_events=10) #, max_events=50) #  
         next(generator) # leave the first event as it is reasonably shifted to low caps
-
-        # 
+        
+        # initialize stats array on which calculations are carried out
         stats = RunningStats(shape=4096)
+   
+        # give out pixelindex (= channel) and gaintype during calculation to maintain overview of progress
+        print(pixelindex, gaintype)
 
-        # define a pixel index.
-        pixelindex = 0
-
-        # build an array with 4096 columns. calculate mean using Max's method
+        # calculate mean using Max's method
         for event in tqdm(generator):
             data = np.full(4096, np.nan)
             stop_cell = event.header.stop_cells[pixelindex]
             roi = event.roi
-            data[:roi] = event.data["low"][pixelindex]
+            data[:roi] = event.data[gaintype][pixelindex]
             stats.add(np.roll(data, stop_cell))
 
-        np.savetxt('offsets.csv', np.column_stack([stats.mean, stats.std]), delimiter=',')
+            #np.savetxt('offsets.csv', np.column_stack([stats.mean, stats.std]), delimiter=',') # give out text file with data
 
-        # plot means
-        plt.figure()
+        # plot means with RMS
+        plt.title("channel %s %s" %(pixelindex, gaintype))
         plt.errorbar(
             np.arange(4096), 
             stats.mean,
             yerr=stats.std, # yerr = y-error bars
             fmt="+", # fmt means format
             markersize=3,
-            capsize=0, # frame bars of error bars
+            capsize=1, # frame bars of error bars
         )
-        # plt.xlim(0,4096)      
-        plt.show()
+        plt.figure()
+        # plt.xlim(0,4096)
+
 
 if __name__ == '__main__':
-    offset_calc('Ped444706_1.dat')
+    for pixelindex in range(8):
+        # calculate mean offset & RMS for every capacitor and plot the data.
+        offset_calc('Ped444706_1.dat', pixelindex, "low")
+        offset_calc('Ped444706_1.dat', pixelindex, "high")
+    plt.show()
