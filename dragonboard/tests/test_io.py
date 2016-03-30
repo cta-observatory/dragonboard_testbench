fbe0e060c8bc1f61b8989c0779066568493b7b38
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-03-30 21:32:01 +0200

    Fix tests for io

--- dragonboard/tests/test_io.py
+++ dragonboard/tests/test_io.py
@@ -1,14 +1,28 @@
-
 def test_reading_v5_1_05():
     from ..io import EventGenerator_v5_1_05
 
     eg = EventGenerator_v5_1_05(
-        'data/fake_pedestal_v5_1_05.dat',
+        'data/random_noise_v5_1_05.dat',
         max_events=10,
     )
 
     repr(eg)
     assert eg.event_size == 32816
+
+    event = next(eg)
+    assert event.roi == 1024
+
+
+def test_reading_v5_1_0B():
+    from ..io import EventGenerator_v5_1_0B
+
+    eg = EventGenerator_v5_1_0B(
+        'data/random_noise_v5_1_0B.dat',
+        max_events=10,
+    )
+
+    repr(eg)
+    assert eg.event_size == 32832
 
     event = next(eg)
     assert event.roi == 1024
