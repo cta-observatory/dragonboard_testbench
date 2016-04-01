a9ae2ff9bc3f2d421292ea853507fd60b2f049b1
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-04-01 11:30:09 +0200

    Add utilities for converting between cell and sample ids

--- dragonboard/__init__.py
+++ dragonboard/__init__.py
@@ -9,3 +9,24 @@ __all__ = [
     'DragonBrowser',
     'RunningStats',
 ]
+
+
+def cell2sample(cell, stop_cell, roi, total_cells=4096):
+    '''
+    Converts a hardware cell to the sample id for given stop_cell and roi
+    Raises a ValueError if the cell is not in the data samples
+    '''
+    assert cell < total_cells
+
+    if stop_cell <= cell < stop_cell + roi:
+        return cell - stop_cell
+
+    if cell < (stop_cell + roi) % total_cells:
+        return total_cells - stop_cell + cell
+
+    raise ValueError('Physical capacitor not in data')
+
+
+def sample2cell(sample, stop_cell, total_cells=4096):
+    ''' Convert a sample id to the physical cell id for given stop_cell '''
+    return (sample + stop_cell) % total_cells
