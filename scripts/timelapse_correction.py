b9150fa7cd7185f2af10fce8d75ff33d2ed152f3
Mario <mario.hoerbe@rub.de>
2016-04-05 22:43:02 +0200

    - set powerlaw suggestion for fit to -1 - added rainbow color scheme for plot. colors indicate relative positions of sample_ids in roi - fit function is now displayed in legend - hard-coded sample_id selection range (experimental) - replaced delta_t_in_ms by time_since_last_readout

--- scripts/timelapse_correction.py
+++ scripts/timelapse_correction.py
@@ -49,6 +49,7 @@ def timelapse_calc(inputdirectory):
 
             time = []
             adc_counts = []
+            rel_pos_in_roi = []
 
             for event in tqdm(
                     iterable=dragonboard.EventGenerator(filename),
@@ -57,23 +58,29 @@ def timelapse_calc(inputdirectory):
                     unit=" events"):
                 stop_cell = event.header.stop_cells[gaintype][pixelindex]
                 if stop_cell <= cell_id < (stop_cell + event.roi):
-                    time.append(event.header.counter_133MHz)
-                    adc_counts.append(int(event[2][pixelindex][gaintype][cell_id - stop_cell]))
+                    sample_id = cell_id - stop_cell
+                    if 0 < sample_id <= 40:
+                        time.append(event.header.counter_133MHz)
+                        adc_counts.append(int(event[2][pixelindex][gaintype][sample_id]))
+                        rel_pos_in_roi.append(100 / event.roi * sample_id)
         except Exception as e:
             print(e)
 
-    delta_t_in_ms = np.diff(time) / 133e4
-    parameters, covariance = curve_fit(f, delta_t_in_ms, adc_counts[1:], maxfev=10000, p0=[1,-0.25,200])
-    x_plot = np.linspace(np.amin(delta_t_in_ms, axis=None, out=None, keepdims=False), np.amax(delta_t_in_ms, axis=None, out=None, keepdims=False))
+    time_since_last_readout = np.diff(time) / 133e4
+    parameters, covariance = curve_fit(f, time_since_last_readout, adc_counts[1:], maxfev=10000, p0=[1,-1,1])
+    x_plot = np.linspace(np.amin(time_since_last_readout, axis=None, out=None, keepdims=False), np.amax(time_since_last_readout, axis=None, out=None, keepdims=False))
     print(parameters)
     
-    plt.scatter(delta_t_in_ms, adc_counts[1:])
-    plt.plot(x_plot, f(x_plot, *parameters), "r-", label="Fit", linewidth=3)
+    plot = plt.scatter(time_since_last_readout, adc_counts[1:], c=rel_pos_in_roi[1:], cmap="rainbow", vmin=0, vmax=100)
+    plt.plot(x_plot, f(x_plot, *parameters), "r-", label="Fit: f(x) = {} * x^{} + {}".format(round(parameters[0],2),round(parameters[1],2),round(parameters[2],2)), linewidth=3)
     plt.xlabel("time between consecutive events / ms")
     plt.ylabel("ADC counts")
     plt.xscale("log")
-    plt.title("Time dependence: cap {} @ {} {} ({})".format(cell_id, pixelindex, gaintype, os.path.basename(filename)))
+    plt.title("Time dependence: cap {} @ {} {} ({})\n".format(cell_id, pixelindex, gaintype, os.path.basename(filename)))
+    colorbar = plt.colorbar(plot)
+    colorbar.set_label("relative position of sample_id in roi / %")
     plt.legend(loc="best")
+
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Dragon Board Time-Dependent Offset Calculation v.1.0')
