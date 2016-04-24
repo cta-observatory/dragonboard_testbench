1cbf65229558a2f90f9a9e978d5d461180b273ea
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-04-24 18:30:36 +0200

    Add Timelapse calibration class

--- dragonboard/__init__.py
+++ dragonboard/__init__.py
@@ -1,8 +1,8 @@
-from .io import read, EventGenerator, max_roi
+from .io import read, EventGenerator
 from .plotting import DragonBrowser
 from .runningstats import RunningStats
+from .utils import cell2sample, sample2cell, cell_in_samples
 
-import numpy as np
 
 __all__ = [
     'read',
@@ -10,28 +10,7 @@ __all__ = [
     'Event',
     'DragonBrowser',
     'RunningStats',
+    'cell2sample',
+    'sample2cell',
+    'cell_in_samples',
 ]
-
-
-def cell2sample(cell, stop_cell, total_cells=max_roi):
-    '''
-    Converts a hardware cell to the sample id for given stop_cell and roi
-
-    This makes no assumption about the roi, so if the returned value is >= roi,
-    the cell was not sampled
-    '''
-    assert np.all(cell < total_cells)
-
-    return (cell - stop_cell) % total_cells
-
-
-def sample2cell(sample, stop_cell, total_cells=max_roi):
-    ''' Convert a sample id to the physical cell id for given stop_cell '''
-    return (sample + stop_cell) % total_cells
-
-
-def cell_in_samples(cell, stop_cell, roi, total_cells=max_roi):
-    ''' Test if the hardware cell was read out for given stop_cell and roi '''
-    assert np.all(cell < total_cells)
-
-    return cell2sample(cell, stop_cell, total_cells) < roi
---
+++ dragonboard/calibration.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+
+from .utils import sample2cell
+
+
+class TimelapseCalibration:
+
+    def __init__(self, filename):
+        self.calib_constants = pd.read_hdf(filename).set_index(
+            ['pixel', 'channel', 'cell']
+        ).sort_index()
+        self.roi = None
+        self.sample = None
+
+    def offset(self, delta_t, a, b, c):
+        o = a * delta_t ** b + c
+        o[np.isnan(o)] = c
+        o[np.isnan(o)] = 0
+
+        return o
+
+    def __call__(self, event):
+        ''' calibrate data in event '''
+        if self.roi is None:
+            self.roi = event.roi
+            self.sample = np.arange(event.roi)
+
+        assert self.roi == event.roi
+
+        for pixel in range(len(event.data)):
+            for channel in event.data.dtype.names:
+                sc = event.header.stop_cells[pixel][channel]
+                cells = sample2cell(self.sample, sc)
+
+                dt = event.time_since_last_readout[pixel][channel]
+                c = self.calib_constants.loc[pixel, channel].loc[cells]
+                event.data[pixel][channel] -= self.offset(dt, c['a'], c['b'], c['c']).astype('>i2')
+
+        return event
