6fa081019664c17997962c9269dc82b07766b5f7
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-04-28 13:37:44 +0200

    Remove plotting and add outputfile/verbosity option to fit_delta_t.py, fix #41

--- scripts/fit_delta_t.py
+++ scripts/fit_delta_t.py
@@ -1,23 +1,21 @@
 '''
 Usage:
-    fit_delta_t.py <inputfile> [options]
+    fit_delta_t.py <inputfile> <outputfile> [options]
 
 Options:
-    -p, --plot              Show plots while fitting
-    -n <n>, --n-jobs=<n>    How many threads to use [default: 1]
+    -n <n>, --n-jobs=<n>     How many threads to use [default: 1]
+    -v <v>, --verbosity=<v>  Verbosity of joblib [default: 5]
 '''
 import pandas as pd
 from scipy.optimize import curve_fit
 from joblib import Parallel, delayed
 import numpy as np
-import matplotlib.pyplot as plt
 import logging
+import os
+import sys
 from docopt import docopt
 
 logging.basicConfig(level=logging.INFO)
-
-fig = None
-x = np.logspace(-4, 0, 250)
 
 
 def f(x, a, b, c):
@@ -44,27 +42,6 @@ def fit(df, cell, plot=False):
         logging.error('Could not fit cell {}'.format(cell))
         return np.full(4, np.nan)
 
-    if plot:
-        global fig, ax, x
-
-        if fig is None:
-            fig = plt.figure()
-            ax = fig.add_subplot(1, 1, 1)
-            fig.tight_layout()
-
-        ax.cla()
-        ax.set_xscale('log')
-        ax.set_xlim(1e-4, 1)
-        ax.scatter(
-            x='delta_t', y='adc_counts', c='sample',
-            cmap='rainbow', linewidth=0, s=4,
-            data=df,
-        )
-        ax.plot(x, f(x, a, b, c))
-
-        fig.canvas.draw()
-        plt.pause(1e-9)
-
     ndf = len(df.index) - 3
     residuals = df['adc_counts'] - f(df['delta_t'], a, b, c)
     chisquare = np.sum(residuals**2) / ndf
@@ -75,13 +52,15 @@ def fit(df, cell, plot=False):
 if __name__ == '__main__':
     args = docopt(__doc__)
 
-    plt.rcParams['figure.figsize'] = (4, 3)
-    plt.ion()
-
-    pool = Parallel(int(args['--n-jobs']), verbose=5)
+    pool = Parallel(int(args['--n-jobs']), verbose=int(args['--verbosity']))
     ids = np.arange(4096)
 
-    with pd.HDFStore('calib_constants.hdf', 'w') as store:
+    if os.path.isfile(args['<outputfile>']):
+        answer = input('Outputfile exists, overwrite? (y / [n]): ')
+        if not answer.lower().startswith('y'):
+            sys.exit()
+
+    with pd.HDFStore(args['<outputfile>'], 'w') as store:
         for pixel in range(8):
             for channel in ('low', 'high'):
                 logging.info('%s  %s', pixel, channel)
@@ -91,9 +70,8 @@ if __name__ == '__main__':
                 )
 
                 by_cell = data.groupby('cell')
-                result = pd.DataFrame(pool(delayed(fit)(
-                    df, name, plot=args['--plot']) for name, df in by_cell
-                    ),
+                result = pd.DataFrame(
+                    pool(delayed(fit)(df, name) for name, df in by_cell),
                     columns=['a', 'b', 'c', 'chisq_ndf']
                 )
                 result['pixel'] = pixel
