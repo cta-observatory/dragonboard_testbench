bb449e8eaaa9a96a6aa6e16a37a1a049cbb7144d
Mario <mario.hoerbe@rub.de>
2016-05-04 11:45:12 +0200

    script to calculate and plot timeseries std_devs based on different calib methods. yet a prototype.

---
+++ scripts/timeseries_full.py
@@ -0,0 +1,90 @@
+'''
+Usage:
+    timeseries_std.py <inputfile> <calc_method_1> <calc_method_2> <calc_method_3> <outputfile> [options]
+
+Options:
+    -p <pixel>       Pixel [default: 0]
+    -c <channel>     Channel [default: low]
+    -n <cores>       Cores to use [default: 1]
+    -v <verbosity>   Verbosity [default: 10]
+    -m <max_events>  Maximum number of Events
+'''
+from dragonboard import EventGenerator
+from dragonboard.calibration import TimelapseCalibration
+from joblib import Parallel, delayed
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from docopt import docopt
+
+
+def calc_data(event):
+    data = {}
+
+    data['uncalib_mean'] = np.mean(event.data[pixel][channel])
+    data['uncalib_std'] = np.std(event.data[pixel][channel])
+
+    event_calib_1 = calib_1(event)
+
+    data['calib_1_mean'] = np.mean(event_calib_1.data[pixel][channel])
+    data['calib_1_std'] = np.std(event_calib_1.data[pixel][channel])
+
+    event_calib_2 = calib_2(event)
+
+    data['calib_2_mean'] = np.mean(event_calib_2.data[pixel][channel])
+    data['calib_2_std'] = np.std(event_calib_2.data[pixel][channel])
+
+    event_calib_3 = calib_3(event)
+
+    data['calib_3_mean'] = np.mean(event_calib_3.data[pixel][channel])
+    data['calib_3_std'] = np.std(event_calib_3.data[pixel][channel])
+
+    return data
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+
+    pixel = int(args['-p'])
+    channel = args['-c']
+
+    calib_1 = TimelapseCalibration(args['<calc_method_1>'])
+    calib_2 = TimelapseCalibration(args['<calc_method_2>'])
+    calib_3 = TimelapseCalibration(args['<calc_method_3>'])
+        
+    events = EventGenerator(
+        args['<inputfile>'],
+        max_events=int(args['-m']) if args['-m'] else None,
+    )
+
+    with Parallel(int(args['-n']), verbose=int(args['-v'])) as pool:
+
+        data = pd.DataFrame(
+            pool(delayed(calc_data)(event) for event in events)
+        )
+
+    plt.style.use('ggplot')
+    fig, ax = plt.subplots()
+
+    data['uncalib_std'].plot.hist(
+        bins=100, range=[0, 60], histtype='step', legend='false',
+        ax=ax, label='Uncalibrated'
+    )
+    data['calib_1_std'].plot.hist(
+        bins=100, range=[0, 60], histtype='step', legend='false',
+        ax=ax, label='Calibrated_M1'
+    )
+    data['calib_2_std'].plot.hist(
+        bins=100, range=[0, 60], histtype='step', legend='false',
+        ax=ax, label='Calibrated_M2'
+    )
+    data['calib_3_std'].plot.hist(
+        bins=100, range=[0, 60], histtype='step', legend='false',
+        ax=ax, label='Calibrated_M3'
+    )
+
+    ax.set_xlabel('Timeseries standard deviation')
+    fig.tight_layout()
+    plt.show()
+
+    data.to_hdf(args['<outputfile>'], 'data')
