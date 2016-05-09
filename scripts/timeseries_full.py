9fe61b8f95f13e771883761d32a9213d3b26d9d5
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-05-09 16:39:22 +0200

    Remove unused options and add options for discarding smaples at start and end of timeseries

--- scripts/timeseries_full.py
+++ scripts/timeseries_full.py
@@ -3,12 +3,12 @@ Usage:
     timeseries_std.py <inputfile> <fit_delta_t.py_output_file> <offset_cell_sample.py_output_file> <outputfile> [options]
 
 Options:
-    -p <pixel>       Pixel [default: 0]
-    -g <gain>        Gain [default: low]
     -n <cores>       Cores to use [default: 1]
     -v <verbosity>   Verbosity [default: 10]
     -m <max_events>  Maximum number of Events
     --skip=<N>       Number of events to skip at start [default: 0]
+    --start=<N>      First sample to consider
+    --end=<N>        Last sample to consider, negative numbers count from end
 '''
 from dragonboard import EventGenerator
 from dragonboard.calibration import TimelapseCalibration
@@ -23,8 +23,10 @@ from docopt import docopt
 from collections import defaultdict
 
 
-def calc_data(event):
+def calc_data(event, start=None, end=None):
     data = defaultdict(dict)
+
+    sl = slice(start, end)
 
     for calib in calibs:
         key = calib.__class__.__name__
@@ -35,10 +37,10 @@ def calc_data(event):
 
                 idx = (event.header.event_counter, pixel, channel)
 
-                data[key + '_mean'][idx] = np.mean(event_calib.data[pixel][channel])
-                data[key + '_std'][idx] = np.std(event_calib.data[pixel][channel])
-                data[key + '_min'][idx] = np.min(event_calib.data[pixel][channel])
-                data[key + '_max'][idx] = np.max(event_calib.data[pixel][channel])
+                data[key + '_mean'][idx] = np.mean(event_calib.data[pixel][channel][sl])
+                data[key + '_std'][idx] = np.std(event_calib.data[pixel][channel][sl])
+                data[key + '_min'][idx] = np.min(event_calib.data[pixel][channel][sl])
+                data[key + '_max'][idx] = np.max(event_calib.data[pixel][channel][sl])
 
     return pd.DataFrame(data)
 
@@ -47,9 +49,6 @@ if __name__ == '__main__':
     args = docopt(
         __doc__, version='Dragon Board Time-Dependent Offset Calculation v.1.0'
     )
-
-    pixel = int(args['-p'])
-    channel = args['-g']
 
     calibs = [
         NoCalibration(),
@@ -70,10 +69,16 @@ if __name__ == '__main__':
     for i in range(int(args['--skip'])):
         next(events)
 
+    start = int(args['--start']) if args['--start'] else None
+    end = int(args['--end']) if args['--end'] else None
+
     with Parallel(int(args['-n']), verbose=int(args['-v'])) as pool:
 
         data = pd.concat(
-            pool(delayed(calc_data)(event) for event in events)
+            pool(
+                delayed(calc_data)(event, start=start, end=end)
+                for event in events
+            )
         )
 
     data.to_hdf(args['<outputfile>'], 'timeseries_full_data')
