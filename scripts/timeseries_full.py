0d2bd4ad1aaf9aa0a2d97453e7211d9b00b3c851
Maximilian Noethe <maximilian.noethe@tu-dortmund.de>
2016-05-09 16:25:01 +0200

    Resolve pushed merge commits in scripts/timeseries_full.py

--- scripts/timeseries_full.py
+++ scripts/timeseries_full.py
@@ -14,11 +14,8 @@ from dragonboard import EventGenerator
 from dragonboard.calibration import TimelapseCalibration
 from dragonboard.calibration import TimelapseCalibrationExtraOffsets
 from dragonboard.calibration import MedianTimelapseExtraOffsets
-<<<<<<< HEAD
 from dragonboard.calibration import MedianTimelapseCalibration
-=======
 from dragonboard.calibration import NoCalibration
->>>>>>> e3819ed300dac4e3a62dbc08e0890d6558036c54
 from joblib import Parallel, delayed
 import numpy as np
 import pandas as pd
@@ -43,18 +40,7 @@ def calc_data(event):
                 data[key + '_min'][idx] = np.min(event_calib.data[pixel][channel])
                 data[key + '_max'][idx] = np.max(event_calib.data[pixel][channel])
 
-<<<<<<< HEAD
-    event_calib_4 = calib_4(event)
-
-    data['calib_4_mean'] = np.mean(event_calib_4.data[pixel][channel])
-    data['calib_4_std'] = np.std(event_calib_4.data[pixel][channel])
-    data['calib_4'] = np.min(event.data[pixel][channel])
-    data['calib_4'] = np.max(event.data[pixel][channel])
-
-    return data
-=======
     return pd.DataFrame(data)
->>>>>>> e3819ed300dac4e3a62dbc08e0890d6558036c54
 
 
 if __name__ == '__main__':
@@ -64,13 +50,6 @@ if __name__ == '__main__':
 
     pixel = int(args['-p'])
     channel = args['-g']
-<<<<<<< HEAD
-    
-    calib_1 = TimelapseCalibration(args['<fit_delta_t.py_output_file>'])
-    calib_2 = TimelapseCalibrationExtraOffsets(offsets_file=args['<offset_cell_sample.py_output_file>'],fits_file=args['<fit_delta_t.py_output_file>'])
-    calib_3 = MedianTimelapseExtraOffsets(args['<offset_cell_sample.py_output_file>']) 
-    calib_4 = MedianTimelapseCalibration(args['<fit_delta_t.py_output_file>'])       
-=======
 
     calibs = [
         NoCalibration(),
@@ -79,9 +58,9 @@ if __name__ == '__main__':
             offsets_file=args['<offset_cell_sample.py_output_file>'],
             fits_file=args['<fit_delta_t.py_output_file>']
         ),
-        MedianTimelapseExtraOffsets(args['<offset_cell_sample.py_output_file>'])
+        MedianTimelapseExtraOffsets(args['<offset_cell_sample.py_output_file>']),
+        MedianTimelapseCalibration(args['<fit_delta_t.py_output_file>']),
     ]
->>>>>>> e3819ed300dac4e3a62dbc08e0890d6558036c54
 
     events = EventGenerator(
         args['<inputfile>'],
@@ -97,37 +76,4 @@ if __name__ == '__main__':
             pool(delayed(calc_data)(event) for event in events)
         )
 
-<<<<<<< HEAD
-    plt.style.use('ggplot')
-    fig, ax = plt.subplots()
-
-    data['uncalib_std'].plot.hist(
-        bins=100, range=[0, 60], histtype='step', legend='false',
-        ax=ax, label='Uncalibrated', color="red"
-    )
-    data['calib_1_std'].plot.hist(
-        bins=100, range=[0, 60], histtype='step', legend='false',
-        ax=ax, label='TimelapseCalibration', color="blue"
-    )
-    data['calib_2_std'].plot.hist(
-        bins=100, range=[0, 60], histtype='step', legend='false',
-        ax=ax, label='TimelapseCalibrationExtraOffsets', color="green"
-    )
-    data['calib_3_std'].plot.hist(
-        bins=100, range=[0, 60], histtype='step', legend='false',
-        ax=ax, label='MedianTimelapseExtraOffsets', color="black"
-    )
-    data['calib_4_std'].plot.hist(
-        bins=100, range=[0, 60], histtype='step', legend='false',
-        ax=ax, label='MedianTimelapseCalibration', color="orange"
-    )
-
-    plt.title("pixel: {}, {}".format(pixel,channel))
-    ax.set_xlabel('Timeseries standard deviation')
-    fig.tight_layout()
-    plt.show()
-
     data.to_hdf(args['<outputfile>'], 'timeseries_full_data')
-=======
-    data.to_hdf(args['<outputfile>'], 'timeseries_full_data')
->>>>>>> e3819ed300dac4e3a62dbc08e0890d6558036c54
