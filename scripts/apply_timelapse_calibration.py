6c726a747f84cb7b5801cbc835dbc804c72cae65
Mario <mario.hoerbe@rub.de>
2016-05-24 16:04:15 +0200

    separate method store

--- scripts/apply_timelapse_calibration.py
+++ scripts/apply_timelapse_calibration.py
@@ -6,6 +6,7 @@ Options:
     -n <cores>       Cores to use [default: 1]
     -v <verbosity>   Verbosity [default: 10]
     -m <max_events>  Maximum number of Events
+    -s <skip>        Events to be skipped [default: 0]
 
 apply TimelapseCalibration for given .dat file(s):
 inputfile: .dat file
@@ -19,39 +20,22 @@ from docopt import docopt
 import pickle
 import os
 
-def calc_data(event):
+
+def calibrate(event):
     """Calibrate data"""
     return calib(event)
 
 
-if __name__ == '__main__':
-    args = docopt(
-        __doc__, version='Dragon Board Timelapse Calibration v.1.0'
-    )
-
-    calib = TimelapseCalibration(args['<fit_constants>'])
-
-    events = EventGenerator(
-        args['<inputfile>'],
-        max_events=int(args['-m']) if args['-m'] else None,
-    )
-
-    with Parallel(int(args['-n']), verbose=int(args['-v'])) as pool:
-
-        data = list(
-            pool(
-                delayed(calc_data)(event) for event in events
-            )
-        )
-
-    file = (args['<outpath>'] + '{}_calib.pickle'.format(os.path.basename(args['<inputfile>'])))
+def store(data):
+    """Store acquired data in pieces of max_data_size"""
+    file = os.path.join(args['<outpath>'], '{}_calib.pickle'.format(os.path.basename(args['<inputfile>'])))
     max_data_size = 10000
 
     for i in range(int(len(data) / max_data_size)):
         try:
             f = open(file, "rb")
             calib_data = []
-            while 1:
+            while True:
                 try:
                     calib_data.append(pickle.load(f))
                 except EOFError:
@@ -64,5 +48,31 @@ if __name__ == '__main__':
                 pickle.dump(data_item, f)
         except:
             pass
-        pickle.dump(data[i * max_data_size:(i + 1) * max_data_size - 1], f)
+        pickle.dump(data[i * max_data_size:(i + 1) * max_data_size], f)
         f.close()
+
+
+if __name__ == '__main__':
+    args = docopt(
+        __doc__, version='Dragon Board Timelapse Calibration v.1.0'
+    )
+
+    calib = TimelapseCalibration(args['<fit_constants>'])
+
+    events = EventGenerator(
+        args['<inputfile>'],
+        max_events=(int(args['-m']) + int(args['-s'])) if args['-m'] else None,
+    )
+
+    for i in range(int(args['-s'])):
+        next(events)
+
+    with Parallel(int(args['-n']), verbose=int(args['-v'])) as pool:
+
+        data = list(
+            pool(
+                delayed(calibrate)(event) for event in events
+            )
+        )
+
+    store(data)
