703bb83da57466e759dfc337fa9488af2029e9d9
Dominik Neise <neised@phys.ethz.ch>
2016-08-03 09:49:37 +0200

    new EventHeaderGenerator for fast reading of headers only

--- dragonboard/__init__.py
+++ dragonboard/__init__.py
@@ -1,4 +1,4 @@
-from .io import read, EventGenerator
+from .io import read, EventGenerator, EventHeaderGenerator
 from .plotting import DragonBrowser
 from .runningstats import RunningStats
 from .utils import cell2sample, sample2cell, cell_in_samples
@@ -9,6 +9,7 @@ __version__ = pkg_resources.require('dragonboard')[0].version
 __all__ = [
     'read',
     'EventGenerator',
+    'EventHeaderGenerator',
     'Event',
     'DragonBrowser',
     'RunningStats',
